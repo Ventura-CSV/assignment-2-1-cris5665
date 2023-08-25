efb97b04ca5f9e0a96c4e36c0bd4dc1847cca79e
“Cristian <ctoledo5665@gmail.comgit config --global user.name cris5665git config --global user.email ctoledo5665@gmail.com>
2023-08-25 16:08:09 -0700

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-
+    num_males = int (input("Enter number of male students: "))
+    num_females = int (input("Enter number of female students: "))
+    totalstudents = num_males + num_females
+    print("Number of total students: ", totalstudents)
+    m_perc = (num_males/totalstudents)*100
+    print("Percent of male students: ", (format(m_perc, '.2f')))
+    f_perc = (num_females/totalstudents)*100
+    print("Percent of female students: ", (format(f_perc, '.2f')))
     """
     ########################################
     # Do not delete the return statement
